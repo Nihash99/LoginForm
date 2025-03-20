cba91e785e01012de736af369107c6b5a080d6b2
Nihash krishna Yanamandala <nihash_yanamandala@Nihashs-Air.attlocal.net>
2025-03-20 11:51:50 -0500

    Database changes

--- src/test/java/com/example/Login_Form/LoginFormApplicationTests.java
+++ src/test/java/com/example/Login_Form/LoginFormApplicationTests.java
@@ -3,11 +3,12 @@ package com.example.Login_Form;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.backend.Login_Form.LoginFormApplication;
+
+@SpringBootTest(classes = LoginFormApplication.class)
 class LoginFormApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
